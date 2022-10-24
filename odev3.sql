a40409e81dc84fb6b5c06e4c023dd8053c371acb
aumitcelik <aumitcelik@gmail.com>
2022-10-24 05:10:01 +0300

    odev3fix

--- odev3.sql
+++ odev3.sql
@@ -10,4 +10,4 @@ SELECT country FROM country WHERE country LIKE '_____%n';
 SELECT title FROM film WHERE title ILIKE '%t%t%t%t%';
 
 -- film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralama
-SELECT * FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate > 2.99;
+SELECT * FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
